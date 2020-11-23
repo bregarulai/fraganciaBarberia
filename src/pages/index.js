7c7cbd3739ccdf0325677d44919395135550bf69
bregarulai <bregarulai@gmail.com>
2020-11-23 14:26:10 -0500

    created footer layout and navigation components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
 import React from "react"
+import { Layout } from "../components"
 
 export default function Home() {
   return (
-    <div className="section">
-      <div className="section-center">
-        <div>
-          <ul>
-            <li>home</li>
-            <li>about</li>
-            <li>mark</li>
-            <li>
-              <a href="#">rambo</a>
-            </li>
-          </ul>
+    <Layout>
+      <div className="section">
+        <div className="section-center">
+          <div>
+            <ul>
+              <li>home</li>
+              <li>about</li>
+              <li>mark</li>
+              <li>
+                <a href="#">rambo</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   )
 }
