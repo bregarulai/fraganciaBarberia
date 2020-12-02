c10d6aca0439190d3e403388cc49c5e7bafff93d
bregarulai <bregarulai@gmail.com>
2020-12-01 21:46:22 -0500

    added buttom to singl image template

--- src/templates/GalleryTemplate.js
+++ src/templates/GalleryTemplate.js
@@ -19,6 +19,14 @@ const GalleryTemplate = ({
         <div className="section-center img-box">
           <Image fluid={fluid} />
         </div>
+        <button
+          className="btn"
+          onClick={() => {
+            typeof window.history !== "undefined" && window.history.go(-1)
+          }}
+        >
+          back
+        </button>
       </Wrapper>
     </Layout>
   )
@@ -47,12 +55,41 @@ const Wrapper = styled.section`
 
     @media screen and (min-width: 900px) {
       margin-top: -12rem;
-      margin-bottom: 5rem;
     }
 
     @media screen and (min-width: 1200px) {
       margin-top: -42rem;
-      margin-bottom: 10rem;
+    }
+  }
+
+  .btn {
+    font-size: 1.8rem;
+    color: var(--color-gray-light-1);
+    background-color: var(--color-primary-blue-dark);
+    padding: 1rem 5rem;
+    border-radius: 5px;
+    text-transform: uppercase;
+    width: 20rem;
+    text-align: center;
+    justify-self: center;
+    transition: all 0.2s;
+    display: block;
+    border: none;
+    transition: all 0.2s;
+    margin: 0 auto;
+    margin-top: 5rem;
+
+    &:hover {
+      background-color: var(--color-primary-red);
+      transform: translateY(-3px);
+      box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.2);
+      outline: none;
+    }
+
+    &:active {
+      transform: translateY(-1px);
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+      outline: none;
     }
   }
 `
